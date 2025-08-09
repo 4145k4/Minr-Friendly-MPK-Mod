87b54ea98c894de347a16b39d5bc304a4fd53056
Alphaesia <creepashadowz@gmail.com>
2025-08-09 14:15:32 +1200

    Update network channel resource location

--- network-api/common/src/main/java/io/github/kurrycat/mpknetapi/common/MPKNetworking.java
+++ network-api/common/src/main/java/io/github/kurrycat/mpknetapi/common/MPKNetworking.java
@@ -7,7 +7,7 @@ import java.util.*;
 public enum MPKNetworking {
     INSTANCE;
 
-    public static final String CHANNEL_NAMESPACE = "mpkmod2";
+    public static final String CHANNEL_NAMESPACE = "minr_mpkmod2";
     public static final String CHANNEL_PATH = "network";
     public static final String MESSENGER_CHANNEL = CHANNEL_NAMESPACE + ":" + CHANNEL_PATH;
     private final Map<UUID, List<MPKPacket>> packetQueue = new HashMap<>();
